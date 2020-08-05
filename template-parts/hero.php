f7c1f859dc208a470c7bc7146a0b5d7335d3f5c9
Lucas Cumsille M <lucas.cumsille@gmail.com>
2020-08-05 11:52:46 +1000

    Menu component
    It is working. Just need to add the correct pages and icons.
    
    Next: carousel

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,7 +1,7 @@
 <?php 
 $image = get_field('hero_background');
 ?>
-	<div class="main-wrapper" style="background-image:url('<?php echo esc_url($image['url']); ?>');">
+	<div class="main-wrapper p-xl" style="background-image:url('<?php echo esc_url($image['url']); ?>');">
 	    <div class="container page-title  text-center gradient py-5">
 			<h1 class="heading"><?php the_field('h1_title'); ?></h1>
 			<p class="text-light mt-5"> <?php the_field('subtitle'); ?> </p>
--- template-parts/homepage-menu.php
+++ template-parts/homepage-menu.php
@@ -1,17 +1,38 @@
-<div class="container">
+
+
+<?php
+    // Get the ID of a given category
+    $category_id_localNews = 5;
+ 
+    // Get the URL of this category
+    $category_link_localNews = get_category_link( $category_id_localNews );
+    $category_name_localNews = get_cat_name( $category_id_localNews );
+
+?>
+
+
+
+
+
+<div class="container pt-5">
+
+        <div class="title-container">
+            <h3 class="section-title text-center text-dark-yellow"> <?php the_field('h3_for_families_title'); ?> </h3>
+        </div>
+
         <div class="d-flex flex-columnn flex-wrap flex-lg-row justify-content-center align-items-center">
 
-            <a class="anchor-card-wrapper"  href="">
+            <a class="anchor-card-wrapper"  href="<?php echo esc_url( $category_link_localNews ); ?>">
                 <div class="menu-card">
                     <div class="upper-menu-card bg-yellow"><img class="menu-icon-homepage mx-auto" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/events 1.svg"></div>
-                    <div class="lower-menu-card"><p>Places to Visit</p></div>
+                    <div class="lower-menu-card"><p> <?php echo $category_name_localNews ?> </p></div>
                 </div>
             </a>
 
             <a class="anchor-card-wrapper" href="">
                 <div class="menu-card">
                     <div class="upper-menu-card bg-yellow"><img class="menu-icon-homepage mx-auto" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/events 1.svg"></div>
-                    <div class="lower-menu-card"><p>Places to Visit</p></div>
+                    <div class="lower-menu-card"><p>Events</p></div>
                 </div>
             </a>
 
@@ -25,6 +46,45 @@
             <a href="" class="anchor-card-wrapper" >
                 <div class="menu-card">
                     <div class="upper-menu-card bg-yellow"><img class="menu-icon-homepage mx-auto" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/events 1.svg"></div>
+                    <div class="lower-menu-card"><p>Places to Visit</p></div>
+                </div>
+            </a>
+
+        </div>
+    </div>
+
+    <div class="container p-s-xl">
+
+        <div class="title-container">
+            <h3 class="section-title text-center text-dark-green"> <?php the_field('h3_for_Parents'); ?> </h3>
+        </div>
+
+        <div class="d-flex flex-columnn flex-wrap flex-lg-row justify-content-center align-items-center">
+
+            <a class="anchor-card-wrapper is--for-parents"  href="<?php echo esc_url( $category_link_localNews ); ?>">
+                <div class="menu-card">
+                    <div class="upper-menu-card bg-green"><img class="menu-icon-homepage mx-auto" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/events 1.svg"></div>
+                    <div class="lower-menu-card"><p> <?php echo $category_name_localNews ?> </p></div>
+                </div>
+            </a>
+
+            <a class="anchor-card-wrapper is--for-parents" href="">
+                <div class="menu-card">
+                    <div class="upper-menu-card bg-green"><img class="menu-icon-homepage mx-auto" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/events 1.svg"></div>
+                    <div class="lower-menu-card"><p>Events</p></div>
+                </div>
+            </a>
+
+            <a href="" class="anchor-card-wrapper is--for-parents" >
+                <div class="menu-card">
+                    <div class="upper-menu-card bg-green"><img class="menu-icon-homepage mx-auto" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/events 1.svg"></div>
+                    <div class="lower-menu-card"><p>Places to Visit</p></div>
+                </div>
+            </a>
+
+            <a href="" class="anchor-card-wrapper is--for-parents" >
+                <div class="menu-card">
+                    <div class="upper-menu-card bg-green"><img class="menu-icon-homepage mx-auto" src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/events 1.svg"></div>
                     <div class="lower-menu-card"><p>Places to Visit</p></div>
                 </div>
             </a>
