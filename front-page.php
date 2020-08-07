e8b530ebd64273df7e37dd3ce586b1ff062fae63
Lucas Cumsille M <lucas.cumsille@gmail.com>
2020-08-07 12:10:57 +1000

    Working on the style of the event grid
    Working so far. Also the event page is not broken

--- front-page.php
+++ front-page.php
@@ -7,10 +7,12 @@
 	get_template_part('template-parts/hero');
 	get_template_part('template-parts/homepage-menu');
 	get_template_part('template-parts/owl-carousel-post');
-	do_shortcode('[ecs-list-events limit="3"]');
-	
 	
 ?>
+
+<div class="container event-home-page">
+	<?php echo do_shortcode('[tribe_events view="list"]'); ?>
+</div>
 
 		<article class="content px-3 py-5 p-md-5">
 
