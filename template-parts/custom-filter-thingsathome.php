d7b946eeb4d6d286162bfee2c88f2d05071865a8
Lucas Cumsille M <lucas.cumsille@gmail.com>
2020-08-19 14:10:33 +1000

    Working on filters

--- template-parts/custom-filter-thingsathome.php
+++ template-parts/custom-filter-thingsathome.php
@@ -12,26 +12,40 @@
 
 <div class="container">
 
-<div class="row">
-	<div class="col-12 col-lg-7">
-		<div class="custom-tab">
-			<button class="tablinks" onclick="openCity(event, 'London')">London</button>
-			<button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+	<div class="row">
+		<div class="col-12 col-lg-7">
+			<div class="custom-tab">
+				<button class="tablinks" onclick="openCity(event, 'London')">London</button>
+				<button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+			</div>
+		</div>
+		<div class="col-12 col-lg-5">
+			<?php echo facetwp_display( 'facet', 'thingsathome_suitable_for' ); ?>
+			<button class="filter-button" type='button' id='hideshow' value='hide/show'><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/filter.svg"> <span>Show all filters</span> </button>
 		</div>
 	</div>
-	<div class="col-12 col-lg-5">
-		<?php echo facetwp_display( 'facet', 'thingsathome_suitable_for' ); ?>
-		<button class="filter-button" type='button' id='hideshow' value='hide/show'><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/filter.svg"> <span>Show all filters</span> </button>
+
+
+
+	<div id="hidden-filters" class="hidden-height">
+
+		<div id="London" class="tabcontent">
+			<div class="row">
+				<div class="col-12 col-lg-7"></div>
+				<div class="col-12 col-lg-5">
+					<h3>London</h3>
+					<?php echo facetwp_display( 'facet', 'play' ); ?>
+				</div>
+			</div>
+
+		</div>
+
+		<div id="Paris" class="tabcontent">
+			<h3>Paris</h3>
+			<?php echo facetwp_display( 'facet', 'sports_and_exercise' ); ?>
+		</div>
+		
 	</div>
-</div>
-
-
-
-<div id="hidden-filters" class="hidden-height">
-	<?php
-		echo facetwp_display( 'facet', 'play' );
-	?>
-</div>
 
 
 </div>
