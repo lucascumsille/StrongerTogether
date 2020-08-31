fe90c62f6cb2d3748dd825382e246b30e16e1a10
Lucas Cumsille M <lucas.cumsille@gmail.com>
2020-09-01 04:39:25 +1000

    filters working with no ajax

---
+++ template-parts/filter/custom-filter-play-and-learn-ajax.php
@@ -0,0 +1,87 @@
+
+<?php do_action('search_filter_query_posts', 445); ?>
+
+<div id="filter-top-container" class="container p-xl">
+
+	<div class="filter-grid">
+		<div class="top-left-column">
+			<div class="flex-wrapper-tab">
+				<a href="<?php echo get_post_type_archive_link( 'thingsathome' ); ?>" class="filter-page-tab left-tab active">
+
+				<?php  $image = get_field('category_icon', $taxonomy . 'category_20');
+					if( !empty( $image ) ): ?>
+						<img class="filter-tab-icon mx-auto" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					<?php endif; ?>
+				<span><?php $term = get_term( 20 , 'thingsathome_category' );
+					echo $term->name; ?></span>
+				</a>
+
+				<a href="<?php echo get_page_link( 435 ); ?>" class="filter-page-tab right-tab">
+
+				<?php  $image = get_field('category_icon', $taxonomy . 'category_21');
+					if( !empty( $image ) ): ?>
+						<img class="filter-tab-icon mx-auto" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+					<?php endif; ?>
+				<span><?php $term = get_term( 21 , 'thingsathome_category' );
+					echo $term->name; ?></span>
+				</a>
+			</div>
+		</div>
+
+		<div id="hidden-filters" class="top-right-column hidden-height">
+
+			<?php echo do_shortcode('[searchandfilter id="445"]'); ?>
+			<?php //do_action("search_filter_query_posts", 445); ?>
+
+		</div>
+
+		<div class="bottom-left-column">
+
+		</div>
+
+		<div class="bottom-right-column">
+			<button class="filter-button" type='button' id='hideshow' value='hide/show'><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/icons/filter.svg"> <span class="filter-button-text">Show all filters FIX MEE ADD "HIDE FILTERS"</span> </button>
+		</div>
+
+
+	</div>
+
+</div>
+
+
+
+
+
+
+	<div id='thingsathome' class="container pb-6">
+
+		<div class="flex-wrapper">
+			
+
+			<?php 
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post(); ?>
+					<article class="flex-item" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<?php get_template_part('template-parts/common/card-things-at-home' ); ?>
+					</article>
+				<?php } // end while
+			} // end if
+			?>
+
+		<?php wp_reset_postdata(); ?>
+
+
+		<?php 
+			// Pagination 
+			$big = 999999999; 
+			echo paginate_links( array(
+				'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
+				'format' => '?paged=%#%',
+				'current' => max( 1, get_query_var('paged') ),
+				'total' => $loop->max_num_pages
+			) );
+		?>
+
+	</div>
+</div>
