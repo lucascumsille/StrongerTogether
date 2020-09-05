1c0aa62384aa62b22cf5b4cb4f7f61bbb41554d3
Lucas Cumsille M <lucas.cumsille@gmail.com>
2020-09-06 07:25:13 +1000

    Event Styling working

--- template-parts/common/card-things-at-home.php
+++ template-parts/common/card-things-at-home.php
@@ -27,7 +27,7 @@
                         
                         <div class="post-carousel-content">
                             <h4 class="text-black"><?php the_title(); ?></h4>
-                            <div class="post-meta-carousel-info justify-content-between">
+                            <div class="post-meta-carousel-info justify-content-between mb-1">
                                 <div class="tag-wrapper"> 
                                     <img class="tag-icon" src="<?php echo esc_url(get_field('category_icon', $category[0])['url']); ?>" > 
                                     <span class="category-tag" style="color:<?php the_field('color_for_category', $category[0]); ?>"><?php  echo $firstCategory->name;  ?></span> 
--- template-parts/contact/contact-repeater.php
+++ template-parts/contact/contact-repeater.php
@@ -1,36 +1,42 @@
-<div class="container-fluid py-4 py-lg-6 bg-green-gradient-local-support">
+<div class="py-4 py-lg-6 bg-green-gradient-local-support">
 <!-- bg-lightest-green -->
-    <div class="container  mb-3 mx-auto">
-        <?php
-        // Check rows exists.
-        if( have_rows('contact_list') ): ?>
-            <div class="contact-wrapper d-flex flex-column">
-            
+    <div class="container mb-3 mx-auto">
+      
+        <div class="single-post-container mx-auto">
+
+        
             <?php
-            // Loop through rows.
-            while( have_rows('contact_list') ) : the_row();
+            // Check rows exists.
+            if( have_rows('contact_list') ): ?>
+                <div class="contact-wrapper d-flex flex-column">
+                
+                <?php
+                // Loop through rows.
+                while( have_rows('contact_list') ) : the_row();
 
-                // Load sub field value.
-                $contact_title = get_sub_field('contact_method');
-                $contact_icon = get_sub_field('contact_icon');
-                $contact_url= get_sub_field('contact_url');
-                $contact_url_display= get_sub_field('contact_url_display');
-                ?>
+                    // Load sub field value.
+                    $contact_title = get_sub_field('contact_method');
+                    $contact_icon = get_sub_field('contact_icon');
+                    $contact_url= get_sub_field('contact_url');
+                    $contact_url_display= get_sub_field('contact_url_display');
+                    ?>
 
-                <a class="contact-repeater-item" href="<?php echo $contact_url ?>">
-                    <img src="<?php echo esc_url($contact_icon['url']); ?>" alt="" class="contact-icon">
-                    <span><?php echo $contact_title; ?>:</span>
-                    <span><?php echo $contact_url_display; ?></span>
-                </a>
+                    <a class="contact-repeater-item" href="<?php echo $contact_url ?>">
+                        <img src="<?php echo esc_url($contact_icon['url']); ?>" alt="" class="contact-icon">
+                        <span><?php echo $contact_title; ?>:</span>
+                        <span><?php echo $contact_url_display; ?></span>
+                    </a>
 
-            <?php // End loop.
-            endwhile;
+                <?php // End loop.
+                endwhile;
 
-        // No value.
-        else :
-            // Do something...
-        endif;
-        ?>
-        </div> <!--  contact-wrapper -->
-    </div>
-</div>
+            // No value.
+            else :
+                // Do something...
+            endif;
+            ?>
+            
+            </div> <!--  contact-wrapper -->
+        </div> <!--  single-post-container  -->
+    </div> <!--  container -->
+</div> <!--  bg-green-gradient-local-support --> 
